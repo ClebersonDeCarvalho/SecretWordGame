2e4471eea72e5061870b6621b2ca83d1821fac3c
ClebersonDeCarvalho <clebersoncarvalhomc5@gmail.com>
2025-01-03 11:30:46 -0300

    Criando componentes e importando palavras

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,38 @@
-
+//Css
 import './App.css'
+
+//React
+import { useCallback,useEffect, useState } from 'react'
+
+//Data
+import {wordsList} from './assets/data/words'
+
+//Components
 import StartScreen from './assets/components/StartScreen'
+import Game from './assets/components/Game'
+import GameOver from './assets/components/GameOver'
+
+
+const stages = [
+  {id:1,name:'start'},
+  {id:2,name:'game'},
+  {id:3,name:'end'}
+]
 
 function App() {
+  
+  const [gameStage, setGameStage] = useState(stages[0].name)
+  const [words] = useState(wordsList)
+
+  console.log(words)
 
   return (
     <>
       <div className='App'>
-        <StartScreen/>
+        {/*Se game state for start ele mostra a tela startScreen */}
+        {gameStage === 'start' && <StartScreen/>}
+        {gameStage === 'game' && <Game/>} 
+        {gameStage === 'end' && <GameOver/>} 
       </div>
       
     </>
