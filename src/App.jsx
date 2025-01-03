c26353158554f62b0c98e5513ff79d54ddd2974d
ClebersonDeCarvalho <clebersoncarvalhomc5@gmail.com>
2025-01-03 11:57:51 -0300

    Alterando estagios do jogo, modificando css global e do gameOver

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,28 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name)
   const [words] = useState(wordsList)
 
-  console.log(words)
+  //Start Secret World game
+  const startGame = ()=>{
+    setGameStage(stages[1].name)
+  }
+
+  //process the letters input
+  const verifyLetter = ()=>{
+    setGameStage(stages[2].name)
+  }
+
+  //restart the game
+  const retry = ()=>{
+    setGameStage(stages[0].name)
+  }
 
   return (
     <>
       <div className='App'>
         {/*Se game state for start ele mostra a tela startScreen */}
-        {gameStage === 'start' && <StartScreen/>}
-        {gameStage === 'game' && <Game/>} 
-        {gameStage === 'end' && <GameOver/>} 
+        {gameStage === 'start' && <StartScreen startGame={startGame}/>}
+        {gameStage === 'game' && <Game verifyLetter={verifyLetter}/>} 
+        {gameStage === 'end' && <GameOver retry={retry}/>} 
       </div>
       
     </>
--- src/assets/components/GameOver.jsx
+++ src/assets/components/GameOver.jsx
@@ -1,9 +1,10 @@
 import './GameOver.css'
 
-const GameOver = () => {
+const GameOver = ({retry}) => {
   return (
     <div>
-      
+      <h1 className='fim'>GameOver</h1>
+      <button onClick={retry}>Recomeçar</button>
     </div>
   )
 }
