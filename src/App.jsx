7bc49847a958f222e401215eb132aef164069a62
ClebersonDeCarvalho <clebersoncarvalhomc5@gmail.com>
2025-01-03 10:47:46 -0300

    Finalizando tela inicial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import StartScreen from './assets/components/StartScreen'
+
+function App() {
+
+  return (
+    <>
+      <div className='App'>
+        <StartScreen/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/assets/data/words.jsx
@@ -0,0 +1,9 @@
+export const wordsList = {
+    carro: ['Motor', 'Porte','Capô', 'Pneu', 'Antena'],
+    fruta: ['Banana', 'Mação', 'Pêra', 'Mamão', 'Laranja'],
+    corpo: ['Braço', 'Perna','Cérebro', 'Pescoço','Olhos'],
+    computação: ['Mouse','Teclado','Monitor','Gabinete'],
+    programação: ['Linguagem','FrameWork','JavaScript','React'],
+    alimento: ['Arroz','Feijão','Carne','Leite','Ovo']
+};
+
